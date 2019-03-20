a5ab14a42d65f45cc2d5f3361e0d58e88772a895
netcorner <netcorner@qq.com>
2019-03-20 13:00:50 +0800

    解决spring boot 2.0不兼容velocity的问题

--- files/VelocityConfig.java
+++ files/VelocityConfig.java
@@ -1,9 +1,13 @@
-package com.netcorner.config;
+package com.netcorner.hotelanalysis.config;
 
+import com.netcorner.hotelanalysis.utils.StringTools;
 import com.netcorner.velocity.VelocityConfigurer;
 import com.netcorner.velocity.VelocityViewResolver;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+
 import java.util.Properties;
 
 /**
@@ -11,11 +15,19 @@ import java.util.Properties;
  */
 @Configuration
 public class VelocityConfig {
+    @Value("${spring.velocity.prefix:/templates/}")
+    private String prefix;
+
+    @Value("${spring.velocity.suffix:.html}")
+    private String suffix;
+
     @Bean
     public VelocityConfigurer getVelocityConfigurer()
     {
+
+
         VelocityConfigurer velocityConfigurer=new VelocityConfigurer();
-        velocityConfigurer.setResourceLoaderPath("/templates/");
+        velocityConfigurer.setResourceLoaderPath(prefix);
         Properties properties=new Properties();
         properties.put("velocimacro.permissions.allow.inline.local.scope",true);
         properties.put("input.encoding","UTF-8");
@@ -25,20 +37,22 @@ public class VelocityConfig {
     }
 
     @Bean
-    public VelocityViewResolver getVelocityViewResolver()
+    public ViewResolver getVelocityViewResolver()
     {
+
         VelocityViewResolver velocityViewResolver=new VelocityViewResolver();
         velocityViewResolver.setCache(false);
-
-        velocityViewResolver.setCache(false);
-        velocityViewResolver.setPrefix("/templates/");
-        velocityViewResolver.setSuffix(".html");
+        velocityViewResolver.setPrefix(prefix);
+        velocityViewResolver.setSuffix(suffix);
         velocityViewResolver.setDateToolAttribute("date");
         velocityViewResolver.setNumberToolAttribute("number");
         velocityViewResolver.setContentType("text/html;charset=UTF-8");
         velocityViewResolver.setExposeSpringMacroHelpers(true);
         velocityViewResolver.setExposeRequestAttributes(true);
         velocityViewResolver.setRequestContextAttribute("request");
+        //velocityViewResolver.setToolboxConfigLocation("/velocity-toolbox.xml");
+        //velocityViewResolver.setViewClass(VelocityToolbox20View.class);
+
 //        Properties properties=new Properties();
 //        velocityViewResolver.setVelocityProperties(properties);
         return velocityViewResolver;
